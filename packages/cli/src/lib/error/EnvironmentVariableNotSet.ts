17390c4d2feb499fef71a5b03be41f464dab2134
Thorben <38816229+tklein1801@users.noreply.github.com>
2026-07-19 20:27:40 +0200

    chore(cli): remove unused code

--- packages/cli/src/lib/error/EnvironmentVariableNotSet.ts
+++
@@ -1,7 +0,0 @@
-import { CustomError } from '@event-specification-gedoens/migration-core';
-
-export class EnvironmentVariableNotSet extends CustomError {
-  constructor(key: string) {
-    super(`The environment variable '${key}' is not available in the current runtime.`);
-  }
-}
