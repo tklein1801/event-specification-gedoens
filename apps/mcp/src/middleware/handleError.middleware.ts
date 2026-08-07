c9c1adeeab679a5dc3a985d9edd2ac44747212c0
Thorben <38816229+tklein1801@users.noreply.github.com>
2026-08-07 11:22:24 +0200

    fix(mcp): add missing argument
    It always failed when the not-used argument wasn't available

--- apps/mcp/src/middleware/handleError.middleware.ts
+++ apps/mcp/src/middleware/handleError.middleware.ts
@@ -1,8 +1,9 @@
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import { config } from '../config';
 import { logger } from '../lib/logger';
 
-export function handleError(err: Error, _req: Request, res: Response): void {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function handleError(err: Error, _req: Request, res: Response, _next: NextFunction): void {
   logger.child({ middleware: 'handleError' }).error('Unhandled error: %s %o', err.name, err);
   res.status(500).json({
     service: config.service,
